ebcd0caad469133be45655e1619ed463fa068098
samPuriwat <sam.puriwat@gmail.com>
2021-02-09 11:31:16 +0700

    Leb 7 : Encapsulation

--- src/LAB7/Student.java
+++ src/LAB7/Student.java
@@ -2,26 +2,25 @@ package LAB7;
 
 public class Student {
     //class attributes
-    static String fuculty = "MT RUTS";
+    static String faculty = "MT RUTS";
     String name;
     int age;
     String major;
 
     //constructor
     // default constructor
-    public Student(){
-        //....
-    }
+
 
     //behavior
     public void introduce(){
         System.out.println("My name is "+name);
         System.out.println("I am "+age+" years old.");
         System.out.println("I studying in "+major);
-        System.out.println("My faculty: "+fuculty);
+        System.out.println("My faculty: "+faculty);
     }
     public void changeFaculty(String newfac){
-        fuculty = newfac;
+
+        faculty = newfac;
     }
 
 }
